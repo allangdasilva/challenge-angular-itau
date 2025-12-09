f5bdc23ebfc166b018cae42017642a79ea772521
Allan Silva <allandasilva33@outlook.com>
2025-12-09 19:11:44 -0300

    Componente InterviewForm criado

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { InterviewForm } from './components/forms/interview-form/interview-form';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, InterviewForm],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('challenge-angular-itau');
